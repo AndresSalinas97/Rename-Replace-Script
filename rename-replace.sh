d785e0f868542221d37adee069ff946ba69c30f3
AndresSalinas97 <andressalinas97@outlook.com>
2022-08-29 12:02:34 +0200

    Complete rewrite to avoid problems with dots and hidden files

--- rename-replace.sh
+++ rename-replace.sh
@@ -1,40 +1,66 @@
 #!/bin/bash
 #
-# A little script to batch-rename files (and directories) by replacing a
+# A little Bash script to batch-rename files (and directories) by replacing a
 # substring in their names.
 #
 # Created by Andrés Salinas Lima on 2020/07/05.
+# Complete rewrite by Andrés Salinas Lima on 2022/08/29.
 
+
+
+#######################################
+# Rename a file/directory by replacing a substring in its name.
+# Arguments:
+#   $1: file path
+#   $2: original substring
+#   $3: replacement substring
+#######################################
+function rename_file() {
+    path=$(dirname "$1")
+    filename_with_extension=$(basename "$1")
+    filename="${filename_with_extension%.*}"
+    extension="${filename_with_extension##*.}"
+
+    # Only if the filename actually contains the string
+    if [[ "$filename" == *"$2"* ]]; then
+        if [[ "$extension" == "$filename" ]]; then
+            # To avoid problems with files without extension / directories
+            dot_extension=""
+        else
+            dot_extension=".""$extension"
+        fi
+
+        # Perform the substring substitution
+        new_filename=${filename//$2/$3}
+
+        echo "Renaming \"$1\""
+        echo "      to \"$path/$new_filename$dot_extension\""
+
+        mv -i "$1" "$path/$new_filename$dot_extension"
+    fi
+}
+
+
+
+# Check command line arguments
 if [[ $# -ne 3 ]]; then
-	echo "Usage: $0 path-to-file-or-folder \"original string\" \"replacement string\""
-	exit
+    echo "ERROR: 3 arguments needed"
+    echo "Usage: $0 path-to-file-or-directory \"original substring\" \"replacement substring\""
+    exit
 fi
 
-# For a single file:
+# A single file
 if [[ -f $1 ]]; then
-	dir_name=$(dirname "$1")
-    base_name=$(basename "$1")
+    rename_file "$1" "$2" "$3"
 
-    new_base_name=$(echo "$base_name" | sed "s/$2/$3/g")
-    # This could also be done entirely with bash string manipulation:
-    # new_base_name=${base_name//$2/$3}
-
-    mv -i "$1" "$dir_name/$new_base_name"
-
-# For entire directories:
+# An entire directory
 elif [[ -d $1 ]]; then
-    find "$1" -depth -name "*$2*" | while IFS= read -r f; do
-        dir_name=$(dirname "$f")
-        base_name=$(basename "$f")
-
-        new_base_name=$(echo "$base_name" | sed "s/$2/$3/g")
-        # This could also be done entirely with bash string manipulation:
-        # new_base_name=${base_name//$2/$3}
-
-        mv -i "$f" "$dir_name/$new_base_name"
+    # Recursively find files/directories (using depth first method) containing $2 (and ignoring hidden files)
+    find "$1" -depth -name "*$2*" -not -path '*/.*' | while IFS= read -r file; do
+        rename_file "$file" "$2" "$3"
     done
 
-# Bad path:
+# Bad path
 else
     echo "ERROR: The entered path doesn't exist"
 fi
